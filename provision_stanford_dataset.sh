af88235f9e0f73155ef4eff3c0521f5367e9cb52
Yakov Danishevsky <yakovdan@gmail.com>
2026-02-19 21:01:28 +0200

    added mc script

---
+++ provision_stanford_dataset.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+set -euo pipefail
+mkdir -p /workspace
+mkdir -p /workspace/repos
+mkdir -p $DATA_DIR
+mkdir -p $OUTPUT_DIR
+
+cd /workspace/repos
+git clone "https://${GH_PAT}@github.com/$GH_USER/Calcium.git"
+cd /workspace/repos/Calcium
+git checkout dev_TrainingGeneralizedDiceRandCrop
+
+cd /workspace/repos
+git clone "https://${GH_PAT}@github.com/$GH_USER/planned-rand-crop.git"
+cd /workspace/repos/planned-rand-crop
+pip install --no-deps .
+
+pip install lightning numpy matplotlib scipy pydicom nibabel tqdm einops boto3 botocore wandb tensorboard
+pip install monai-weekly
+
+wandb login $WANDB_KEY
+
+cd /venv/main/bin
+wget https://github.com/Backblaze/B2_Command_Line_Tool/releases/latest/download/b2-linux
+chmod +x ./b2-linux
+b2-linux account authorize $B2_KEY_ID $B2_APP_KEY
+cd $DATA_DIR
+b2-linux file download b2://calcium-dataset/stanford-aimi-archive/binary_dataset.zip binary_dataset.zip
+unzip binary_dataset.zip
